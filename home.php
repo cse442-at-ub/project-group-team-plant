49142fcc9ce58df9cf110562eaa62e8022fe77b1
smauels <66911986+smauels@users.noreply.github.com>
2023-09-22 22:09:32 -0400

    Update home.php

--- home.php
+++ home.php
@@ -7,11 +7,11 @@
 
 Input a zip-code, soil-composition and/or elevation below then press the button to be recommended<br>
 plants based on the inputs, if an input is left blank then that input will not be apart of the search.<br><br>
-Zip-code: <input type="text" name="zip" value="<?php echo $zip;?>"><br><br>
+Zip-code: <input type="text" name="zip" value=""><br><br>
 
-Soil-composition: <input type="text" name="soil" value="<?php echo $soil;?>"><br><br>
+Soil-composition: <input type="text" name="soil" value=""><br><br>
 
-Elevation: <input type="text" name="elev" value="<?php echo $elev;?>"><br><br>
+Elevation: <input type="text" name="elev" value=""><br><br>
 
 <form method="post">
     <input type="submit" name="recommend_button"
